592dec2761999f910ae61b78828bac135ddbe958
dprofili <dprofili@gmail.com>
2018-06-01 15:39:14 -0400

    Fix exercise script

--- getup.py
+++ getup.py
@@ -1,5 +1,5 @@
 # Motivates the user to get up every once in a while and get ripped
 import random
-things = ['sit ups', 'leg raises', 'push ups', 'jumping jacks',
-          'leg circles', 'lunges', 'squats']
-print('Get up and do' + random.randint(1, 40) + things.index(random.randint(0, len(things))
+things = ['sit ups', 'leg raises', 'push ups', 'jumping jacks', 'leg circles', 'lunges', 'squats']
+print('Get up and do ' + str(random.randint(1, 40)) + ' ' + things[random.randint(0, len(things) - 1)])
+input('? ')
