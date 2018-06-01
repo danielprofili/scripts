b351923ca6047578116b955cfa7c818e20412626
dprofili <dprofili@gmail.com>
2018-06-01 15:34:46 -0400

    Add exercise script

---
+++ getup.py
@@ -0,0 +1,5 @@
+# Motivates the user to get up every once in a while and get ripped
+import random
+things = ['sit ups', 'leg raises', 'push ups', 'jumping jacks',
+          'leg circles', 'lunges', 'squats']
+print('Get up and do' + random.randint(1, 40) + things.index(random.randint(0, len(things))
